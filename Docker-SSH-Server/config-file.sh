5f38dfcb767baed3816043810d5854979722868a
LiliwoL <github@liliwol.fr>
2023-03-19 13:36:11 +0100

    docker SSH

--- Docker-SSH-Server/config-file.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-echo "**** installing ffmpeg ****"
-echo "User1"
-useradd -G admin user1
-echo user1:new_password | chpasswd
-
-echo "User2"
-useradd -G admin user2
-echo user2:new_password2 | chpasswd
